8c8e5c6d8ede2126d6e9e947f6849250f27608ba
YairMoller <yairmoller@hilma.tech>
2024-11-20 13:23:15 +0200

    handle get request for username

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require("express");
+const fs = require("fs");
+const path = require("path");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/:username", function (req, res, next) {
+    console.log("req.params.username: ", req.params.username);
+    let filesArr;
+    fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
+        if (err) return console.log(err);
+        filesArr = files.map((file) => {
+            return {
+                name: file.name,
+                type: file.isDirectory() ? "folder" : "file",
+            };
+        });
+        res.json(filesArr);
+    });
+    res.send(JSON.stringify(filesArr));
 });
 
 module.exports = router;
