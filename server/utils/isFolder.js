bc54318976c1f7edc2a664352cf78e7cad44ff73
YairMoller <yairmoller@hilma.tech>
2024-11-20 21:02:19 +0200

    add basic rest api function

--- server/utils/isFolder.js
+++ server/utils/isFolder.js
@@ -1,8 +1,20 @@
 const fs = require("node:fs/promises");
 
 const isFolder = async (dir) => {
-    console.log("dir: ", dir);
-    return (await fs.stat(dir)).isDirectory();
+    try {
+        return (await fs.stat(dir)).isDirectory();
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+};
+const doesExist = async (dir) => {
+    try {
+        await fs.stat(dir);
+        return true;
+    } catch (err) {
+        return false;
+    }
 };
 
-module.exports = isFolder;
+module.exports = { isFolder, doesExist };
