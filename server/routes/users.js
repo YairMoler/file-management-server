f2c3aea2084409354197e430a00b6deec3d13018
YairMoller <yairmoller@hilma.tech>
2024-11-20 21:28:59 +0200

    add default folder get

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,8 +7,15 @@ const { isFolder, doesExist } = require("../utils/isFolder");
 var router = express.Router();
 
 /* GET method*/
+router.get("/:username", async (req, res, next) => {
+    const dirPath = path.normalize(`${__dirname}/../users/${req.params.username}`);
+    if (!(await doesExist(dirPath))) return res.status(404).send("does not exist");
+    sendDir(res, dirPath);
+});
+
 router.get("/:username/*", async (req, res, next) => {
     const dirPath = path.normalize(`${__dirname}/../users/${req.url}`);
+    console.log("dirPath: ", dirPath);
     if (!(await doesExist(dirPath))) return res.status(404).send("does not exist");
     if (await isFolder(dirPath)) {
         sendDir(res, dirPath);
